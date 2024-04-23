ec8dab52238b9788a429725cf0ef71a7d08de374
Saw Kyaw Myint <scm.sawkyawmyint@gmail.com>
2024-04-23 13:59:53 +0630

    vue package

---
+++ vue-package-button/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <VueElegentButton />
+    <RedButton />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import VueElegentButton from "./components/Button/VueElegentButton.vue";
+import RedButton from "./components/Button/RedButton.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    VueElegentButton,
+    RedButton,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-package-testing/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <VueElegentButton />
+    <RedButton />
+    <GitVueElegentButton />
+    <GitRedButton />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import {
+  RedButton,
+  VueElegentButton,
+} from "@scm-saw-kyaw-myint/vue-package-button/dist/button/DesignButton.umd";
+
+import {
+  RedButton as GitRedButton,
+  VueElegentButton as GitVueElegentButton,
+} from "vue-package-button/dist/button/DesignButton.umd";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    VueElegentButton,
+    RedButton,
+    GitRedButton,
+    GitVueElegentButton,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
